d58ca2e9756d35fa0507329b342b1e87ad4a6f56
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2023-07-12 13:09:47 +0800

    add autogenerate for user model

---
+++ migrations/versions/2023_07_12_1307-7562ce4ac0cf_create_user_table.py
@@ -0,0 +1,44 @@
+"""create_user_table
+
+Revision ID: 7562ce4ac0cf
+Revises: ec1c57adbf5e
+Create Date: 2023-07-12 13:07:03.004525
+
+"""
+from alembic import op
+
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7562ce4ac0cf'
+down_revision = 'ec1c57adbf5e'
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    op.create_table(
+        'user',
+        sa.Column('id', sa.BIGINT(), nullable=False, primary_key=True),
+        sa.Column('last_name', sa.VARCHAR(length=100)),
+        sa.Column('first_name', sa.VARCHAR(length=50)),
+        sa.Column('middle_name', sa.VARCHAR(length=100)),
+        sa.Column('username', sa.VARCHAR(length=100)),
+        sa.Column('email', sa.VARCHAR(length=255)),
+        sa.Column('password', sa.VARCHAR(length=250)),
+        sa.Column('contact_number', sa.VARCHAR(length=20)),
+        sa.Column('status', sa.Enum('free', 'basic', 'starter', 'premium')),
+        sa.Column('created_at', sa.DATETIME(), nullable=False, server_default=sa.text("CURRENT_TIMESTAMP")),
+        sa.Column('updated_at', sa.DATETIME(), nullable=False, server_default=sa.text("CURRENT_TIMESTAMP")),
+    )
+
+    op.create_table(
+        'user_role_association',
+        sa.Column('user_id', sa.BIGINT(), sa.ForeignKey('user.id', ondelete='CASCADE', onupdate='CASCADE')),
+        sa.Column('role_id', sa.BIGINT(), sa.ForeignKey('role.id', ondelete='CASCADE', onupdate='CASCADE'))
+    )
+
+def downgrade() -> None:
+    op.drop_table('user_role_association')
+    op.drop_table('role')
+    op.drop_table('user')
